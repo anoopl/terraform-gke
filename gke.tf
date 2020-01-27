a89548247d0e980bc9673546f97f2934556a8c22
Anoop <anoopl@users.noreply.github.com>
2020-01-27 15:38:56 +0400

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -16,7 +16,7 @@ resource "google_container_cluster" "kubernetes" {
   private_cluster_config {
     enable_private_nodes = "true"
     master_ipv4_cidr_block = "${var.master_cidr}"
-    #enable_private_endpoint = "true"
+    enable_private_endpoint = "true"
   }
   master_authorized_networks_config  {
     cidr_blocks {
